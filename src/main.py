d16a900c9f583b3728af492d55fd71d9d0a7fd2a
Griffin <you@example.com>
2021-05-08 13:37:06 -0400

    started main

---
+++ src/main.py
@@ -0,0 +1,22 @@
+##########################
+# Griffin Reichert
+# CSE 347 Final Project
+#########################
+
+# imports
+import numpy as np
+import idx2numpy    # used to handle mnist files
+
+
+# main program
+mnist_fp = '../data/MNIST/'
+
+# print("\nWelcome to my classification program!")
+# print("  [1] XGBoost")
+# print("  [2] Convolutional Neural Network")
+# print("  [3] Deep Neural Network")
+# inpt = input("Please enter the number of the algorithm you wish to use: ")
+# while inpt != "1" and inpt != "2" and inpt != 'q':
+#     inpt = input("Whoops! Please only enter '1' or '2' ('q' to quit): ")
+
+# if inpt != 'q':
